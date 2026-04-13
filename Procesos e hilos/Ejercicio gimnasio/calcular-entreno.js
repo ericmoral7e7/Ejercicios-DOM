dfe128ddb87a9ff9dff2be6338c5b1005583ccb7
eric <eric.moral.7e7@itb.cat>
2026-04-13 12:10:46 +0200

    Update workout calculation logic and server endpoint to handle weight and height parameters

--- Procesos e hilos/Ejercicio gimnasio/calcular-entreno.js
+++ Procesos e hilos/Ejercicio gimnasio/calcular-entreno.js
@@ -1,11 +1,34 @@
 import { workerData, parentPort } from 'worker_threads';
 
 // Recibe los datos del hilo principal
-const { nombre } = workerData;
+const { nombre, peso, altura } = workerData;
 
-// Hace la tarea pesada
-setTimeout(() => {
-    }, 2000);
+const start = Date.now();
+while (Date.now() - start < 6000) {
+    // bucle bloqueante intencional
+}
+
+let imc = peso / (altura * altura)
+let calorias = 0
+let plan = ""
+
+if (imc < 18.5) {
+    plan = "Volumen";
+    calorias = 3000;
+} else if (imc >= 18.5 && imc < 25) {
+    plan = "Mantenimiento";
+    calorias = 2500;
+} else {
+    plan = "Definición";
+    calorias = 2000;
+}
+
+const resultado = {
+    nombre: nombre,
+    imc: imc,
+    plan: plan,
+    calorias: calorias
+}
 
 // Devuelve el resultado al hilo principal
 parentPort.postMessage({ resultado });
--- Procesos e hilos/Ejercicio gimnasio/servidor.js
+++ Procesos e hilos/Ejercicio gimnasio/servidor.js
@@ -2,37 +2,46 @@ import express from 'express';
 import { Worker } from 'worker_threads';
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 const server = app.listen(PORT, () => {
     console.log(`Worker ${process.pid} escuchando en http://localhost:${PORT}/`);
 });
 
+let entrenosActivos = 0;
 
 app.get('/entreno/:nombre', (req, res) => {
-    const nombre = req.params.nombre
 
+    entrenosActivos++;
+
+    const nombre = req.params.nombre
+    const peso = Number(req.query.peso)
+    const altura = Number(req.query.altura)
+
+    console.log(`${nombre} ${peso} ${altura}`)
     // Crea el worker y le pasa los datos
     const worker = new Worker('./calcular-entreno.js', {
-        workerData: { nombre }            // ← pasa objetos directamente (no solo strings)
+        workerData: { nombre, peso, altura }            // ← pasa objetos directamente (no solo strings)
     });
 
     // Escucha el resultado
     worker.on('message', (datos) => {
+        entrenosActivos--;
         res.json(datos);                   // → { resultado: 15 }
     });
 
     // Gestiona errores
     worker.on('error', (err) => {
+        entrenosActivos--;
         res.status(500).json({ error: err.message });
     });
 
 });
 
 app.get('/ping', (req, res) => {
-    res.json(JSON.parse({ servidor: "vivo" }));
+    res.json({ estado: "Activo" });
 })
 
 app.get('/usuarios-activos', (req, res) => {
-    res.json(JSON.parse({ servidor: "vivo" }));
+    res.json({entrenosActivos: entrenosActivos});
 })
